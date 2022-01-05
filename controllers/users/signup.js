327a6f1d052ee92c804fd931065027e26944e147
Antonina Muravska <at.tonya@gmail.com>
2022-01-05 23:00:46 +0200

    added signup and login

---
+++ controllers/users/signup.js
@@ -0,0 +1,27 @@
+const { User } = require('../../models/user');
+const { Conflict } = require('http-errors');
+const bcrypt = require('bcryptjs');
+
+const signup = async (req, res) => {
+  const { email, password, subscription } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw new Conflict(`User with ${email} is already exist`);
+  }
+
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const result = await User.create({ email, password: hashPassword });
+  res.status(201).json({
+    status: 'success',
+    code: 201,
+    data: {
+      user: {
+        email,
+        subscription,
+      },
+    },
+  });
+};
+
+module.exports = signup;
